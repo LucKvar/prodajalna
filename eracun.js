7d6d1046ddd266c91684c3c638bf28cf5815328f
Lucijan Kvar <lk5628@student.uni-lj.si>
2016-05-08 12:40:49 +0000

    Uspešna preusmeritev

--- eracun.js
+++ eracun.js
@@ -47,6 +47,7 @@ function davcnaStopnja(izvajalec, zanr) {
 
 // Prikaz seznama pesmi na strani
 streznik.get('/', function(zahteva, odgovor) {
+  if(zahteva.session.uspelaPrijava) {
   pb.all("SELECT Track.TrackId AS id, Track.Name AS pesem, \
           Artist.Name AS izvajalec, Track.UnitPrice * " +
           razmerje_usd_eur + " AS cena, \
@@ -60,6 +61,7 @@ streznik.get('/', function(zahteva, odgovor) {
           GROUP BY Track.TrackId \
           ORDER BY steviloProdaj DESC, pesem ASC \
           LIMIT 100", function(napaka, vrstice) {
+    
     if (napaka)
       odgovor.sendStatus(500);
     else {
@@ -67,7 +69,10 @@ streznik.get('/', function(zahteva, odgovor) {
           vrstice[i].stopnja = davcnaStopnja(vrstice[i].izvajalec, vrstice[i].zanr);
         odgovor.render('seznam', {seznamPesmi: vrstice});
       }
-  })
+    })
+  } else {
+      odgovor.redirect('/prijava');
+    }
 })
 
 // Dodajanje oz. brisanje pesmi iz košarice
